fcdc961d34ff818de317c57cca27b8e8b9e61af4
sylvainSUPINTERNET <sylvain.joly@supinternet.fr>
2021-11-13 00:00:46 +0100

    WIP

--- pages/index.tsx
+++ pages/index.tsx
@@ -9,6 +9,7 @@ import { useEffect, useState } from 'react';
 import { url } from 'inspector';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import Link from 'next/link'
 
 
 const Home = ({ task, storeTarget }: { task: any, storeTarget: any }) => {
@@ -103,9 +104,9 @@ const Home = ({ task, storeTarget }: { task: any, storeTarget: any }) => {
         </div>
 
         <div className="flex mt-3 mb-3 justify-center shadow-lg">
-          <a className="m-2 
+          <Link className="m-2 
           bg-gradient-to-r from-red-400 to-blue-500 hover:from-pink-500 hover:to-yellow-500
-          text-white font-bold py-5 px-4 rounded text-4xl mt-5 mb-10 " href="/payment/checkout" >Je commande</a>
+          text-white font-bold py-5 px-4 rounded text-4xl mt-5 mb-10 " href="/payment/checkout" >Je commande</Link>
           {/* <button className="m-2 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full">Type : </button> */}
         </div>
         {
@@ -143,7 +144,7 @@ const Home = ({ task, storeTarget }: { task: any, storeTarget: any }) => {
           
            <p className="font-bold">sylvain.joly00@gmail.com</p>
 
-            <a href="/CGU" className="mt-2">CGU</a>
+            <Link href="/CGU">CGU</Link>
           </div>
         </div>
       </div>
